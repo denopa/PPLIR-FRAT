c4d8bf201fc959bdf4ceacbe8e647139538b1eb5
Patrick <nonneville@october.eu>
2021-02-21 20:05:58 +0100

    Initial Commit

---
+++ Shared/PPLIR_FRATApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPLIR_FRATApp.swift
+//  Shared
+//
+//  Created by October on 21/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PPLIR_FRATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
