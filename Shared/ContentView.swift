822c8a49f14ade22e8ad8b5da71760905fdce1a2
Patrick <nonneville@october.eu>
2021-02-21 21:50:00 +0100

    tab structure

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Hello, world!")
+                .padding()
+                .tabItem {
+                            Image(systemName: "house")
+                            Text("Home")
+                }
+            Text("Pilot")
+                .padding()
+                .tabItem {
+                            Image(systemName: "person")
+                            Text("Pilot")
+                }
+            Text("Airplane")
+                .padding()
+                .tabItem {
+                            Image(systemName: "paperplane")
+                            Text("Airplane")
+                }
+            Text("Environment")
+                .padding()
+                .tabItem {
+                            Image(systemName: "cloud.bolt")
+                            Text("Environment")
+                }
+            Text("Pressure")
+                .padding()
+                .tabItem {
+                            Image(systemName: "scalemass")
+                            Text("Pressure")
+                }
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
