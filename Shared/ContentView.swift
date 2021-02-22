d26fa4400284ba0491a204434837657d4203346a
Patrick <nonneville@october.eu>
2021-02-22 22:49:48 +0100

    pilot form: iPad compatible

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State private var hoursInType: Double = 0
     @State private var recentHoursInType: Double = 0
-    @State private var monthsSinceTraining: Double = 0
+    @State private var monthsSinceTraining: Double = 12
     @State private var IRRatedCopilot: Bool = false
     @State private var license = 0
     let licenses = ["PPL","Instructor","CPL","ATPL"]
@@ -38,66 +38,70 @@ struct ContentView: View {
                             Image(systemName: "house")
                             Text("Home")
                 }
-            NavigationView {
-                Form {
-                    Section(header: Text("Experience")) {
-                        //TextField("Hours in type", text: $hoursInType)
-                        HStack {
-                            Text("Hours in type")
-                            Spacer()
-                            Text("\(hoursInType, specifier: "%.00f")\(hoursInType==500 ? "+" : "")")
-                        }
-                        HSlider(value: $hoursInType, in: 0...500, step: 25, track:
-                                    LinearGradient(gradient: Gradient(colors: [.red, .orange, .green]), startPoint: .leading, endPoint: .trailing)
-                                .frame(height: 3)
-                                .cornerRadius(4)
-                        )
-                        HStack {
-                            Text("Hours in this aircraft (last 90 days)")
-                            Spacer()
-                            Text("\(recentHoursInType, specifier: "%.00f")\(recentHoursInType==30 ? "+" : "")")
-                        }
-                        HSlider(value: $recentHoursInType, in: 0...30, step: 5, track:
-                                    LinearGradient(gradient: Gradient(colors: [.red, .orange, .green]), startPoint: .leading, endPoint: .trailing)
-                                .frame(height: 3)
-                                .cornerRadius(4)
-                        )
-                        HStack {
-                            Text("Months since last training")
-                            Spacer()
-                            Text("\(monthsSinceTraining, specifier: "%.00f")\(monthsSinceTraining==12 ? "+" : "")")
-                        }
-                        HSlider(value: $monthsSinceTraining, in: 0...12, step: 1, track:
-                                    LinearGradient(gradient: Gradient(colors: [.green, .orange,.red]), startPoint: .leading, endPoint: .trailing)
-                                .frame(height: 3)
-                                .cornerRadius(4)
-                        )
-                        Toggle(isOn: $IRRatedCopilot) {
-                            Text("Instructor or IR pilot in copilot's seat? ")
-                        }
-                    }
-                    Section(header: Text("Licenses, Current and Proficient Ratings")) {
-                        Picker(selection: $license, label:Text("License")) {
-                            ForEach(0..<licenses.count) {
-                                Text(self.licenses[$0])
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                        Picker(selection: $rating, label:Text("rating")) {
-                            ForEach(0..<ratings.count) {
-                                Text(self.ratings[$0])
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
+            Form {
+                Section() {
+                    HStack {
+                        Text("Pilot").font(.largeTitle)
+                        Spacer()
+                        Text("\(pilotPoints, specifier: "%.00f") points")
                     }
                 }
-                .navigationTitle("Pilot points: \(pilotPoints, specifier: "%.00f")")
+                Section(header: Text("Experience")) {
+                    //TextField("Hours in type", text: $hoursInType)
+                    HStack {
+                        Text("Hours in type")
+                        Spacer()
+                        Text("\(hoursInType, specifier: "%.00f")\(hoursInType==500 ? "+" : "")")
+                    }
+                    HSlider(value: $hoursInType, in: 0...500, step: 25, track:
+                                LinearGradient(gradient: Gradient(colors: [.red, .orange, .green]), startPoint: .leading, endPoint: .trailing)
+                            .frame(height: 3)
+                            .cornerRadius(4)
+                    )
+                    HStack {
+                        Text("Hours in this aircraft (last 90 days)")
+                        Spacer()
+                        Text("\(recentHoursInType, specifier: "%.00f")\(recentHoursInType==30 ? "+" : "")")
+                    }
+                    HSlider(value: $recentHoursInType, in: 0...30, step: 5, track:
+                                LinearGradient(gradient: Gradient(colors: [.red, .orange, .green]), startPoint: .leading, endPoint: .trailing)
+                            .frame(height: 3)
+                            .cornerRadius(4)
+                    )
+                    HStack {
+                        Text("Months since last training")
+                        Spacer()
+                        Text("\(monthsSinceTraining, specifier: "%.00f")\(monthsSinceTraining==12 ? "+" : "")")
+                    }
+                    HSlider(value: $monthsSinceTraining, in: 0...12, step: 1, track:
+                                LinearGradient(gradient: Gradient(colors: [.green, .orange,.red]), startPoint: .leading, endPoint: .trailing)
+                            .frame(height: 3)
+                            .cornerRadius(4)
+                    )
+                    Toggle(isOn: $IRRatedCopilot) {
+                        Text("Instructor or IR pilot in copilot's seat? ")
+                    }
+                }
+                Section(header: Text("Licenses, Current and Proficient Ratings")) {
+                    Picker(selection: $license, label:Text("License")) {
+                        ForEach(0..<licenses.count) {
+                            Text(self.licenses[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    Picker(selection: $rating, label:Text("rating")) {
+                        ForEach(0..<ratings.count) {
+                            Text(self.ratings[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
             }
-                .padding()
-                .tabItem {
-                            Image(systemName: "person")
-                    Text("Pilot: \(pilotPoints, specifier: "%.00f")")
-                }
+            .padding()
+            .tabItem {
+                        Image(systemName: "person")
+                Text("Pilot: \(pilotPoints, specifier: "%.00f")")
+            }
             Text("Airplane")
                 .padding()
                 .tabItem {
@@ -116,13 +120,12 @@ struct ContentView: View {
                             Image(systemName: "waveform.path.ecg")
                             Text("Pressure")
                 }
-            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            
+        ContentView().environment(\.colorScheme, .light)
     }
 }
